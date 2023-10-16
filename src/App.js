ff949910898cca4166b024b5f1481ac5087b477f
Ayush Bhowmick <109886923+Ayush-Bhowmick@users.noreply.github.com>
2023-10-16 16:28:46 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 
 function App() {
+  //way of accessing variable
+  const userName="Chai and Biscuit"
   return (
     <div className="App">
-      <h1> Hiiiiiiiiiiiiiii AYUSH HERE </h1>
+      <h1> Hiiiiiiiiiiiiiii AYUSH HERE, wanna have  {userName} </h1>
       <ul>
         <li>AYUSH</li>
         <li>maat ho</li>
